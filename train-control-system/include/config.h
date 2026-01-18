432b3679c0bfabd8784f33c9bf5e7a27dfd3b003
Zachary B <96999736+zachnb@users.noreply.github.com>
2026-01-18 20:36:45 +0900

    Format config.h for consistent spacing

--- train-control-system/include/config.h
+++ train-control-system/include/config.h
@@ -1,7 +1,8 @@
 #pragma once
-#define MAX_TRAINS 3
-#define MAX_STATIONS 4
-#define SAFE_DISTANCE 50
-#define MAX_SPEED 40
-#define DWELL_TIME 3
-#define TRACK_END 600
+
+#define MAX_TRAINS     3
+#define MAX_STATIONS   4
+#define SAFE_DISTANCE  50
+#define MAX_SPEED      40
+#define DWELL_TIME     3
+#define TRACK_END      600
