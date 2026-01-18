15ca9a2ec169d0b16fad9c58b9dec30cb62491e3
Zachary B <96999736+zachnb@users.noreply.github.com>
2026-01-18 20:37:58 +0900

    Format Station struct for better readability

--- train-control-system/include/station.h
+++ train-control-system/include/station.h
@@ -1,3 +1,9 @@
 #pragma once
-typedef struct { const char* name; int position; int occupied_by; } Station;
+
+typedef struct {
+    const char* name;
+    int position;
+    int occupied_by;
+} Station;
+
 extern Station stations[];
